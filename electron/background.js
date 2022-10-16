a6a9628ef64997447758ef2ad39b4243799e6af5
Hiram <>
2022-10-16 11:39:39 +0800

    Modify mac exit

--- electron/background.js
+++ electron/background.js
@@ -54,7 +54,11 @@ app.whenReady().then(() => {
 // 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此，通常对程序和它们在
 // 任务栏上的图标来说，应当保持活跃状态，直到用户使用 Cmd + Q 退出。
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  } else if (process.platform === 'darwin') {
+    app.exit()
+  }
 })
 
 // 在这个文件中，你可以包含应用程序剩余的所有部分的代码，
